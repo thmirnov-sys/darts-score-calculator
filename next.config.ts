c24e596e776f73bc53c24c8598a32cc91e5c9504
thmirnov-sys <thmirnov@gmail.com>
2026-07-10 14:57:30 +0300

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  devIndicators: false,
+  poweredByHeader: false,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
